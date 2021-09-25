d79b3621403dabb8acd91dccbd400d829971b9fd
harinivanmeeganathan <harinivanmeeganathan@gmail.com>
2021-09-25 19:23:04 +0530

    Server changes

--- Client/src/components/form/form.component.jsx
+++ Client/src/components/form/form.component.jsx
@@ -1,35 +1,45 @@
-import React from 'react';
+import React, { useState} from 'react';
 import emailjs from 'emailjs-com';
+import axios from 'axios';
 import './form.styles.scss';
 
-function Formpage({toggle}) {
+function Formpage() {
+    const initialState = { name: 'hehe', email: 'test@mail.com', message: 'work now'};
+    const [value, setValue] = useState({ ...initialState });
 
-    function sendEmail(e) {
-      e.preventDefault();
-  
-      emailjs.sendForm('service_k7m1nip', 'template_oxdttxq', e.target, 'user_DLfMVlMooaPHkOEDI7oh7')
-        .then((result) => {
-            console.log(result.text);
-            console.log(e);
-        }, (error) => {
-            console.log(error.text);
-   
-        });
-        e.target.reset();
+    const handleChange = (event) => {
+        setValue({ ...value, [event.target.name]: event.target.value });
     }
+    const submitEmail = async (e) => {
+        e.preventDefault();
+        axios.get('/api/mail', { ...value }).then(res => console.log(res));
+    }
+    // function sendEmail(e) {
+    //   e.preventDefault();
+  
+    //   emailjs.sendForm('service_k7m1nip', 'template_oxdttxq', e.target, 'user_DLfMVlMooaPHkOEDI7oh7')
+    //     .then((result) => {
+    //         console.log(result.text);
+    //         console.log(e);
+    //     }, (error) => {
+    //         console.log(error.text);
+   
+    //     });
+    //     e.target.reset();
+    // }
 return(
 <div className = 'form'>
 
-    <form className = 'contact-form' onSubmit = {sendEmail} method = 'post'>
+    <form className = 'contact-form' onSubmit = {submitEmail} method = 'post'>
         <div className = 'in-form'>
-            <input className = 'input-text' type = 'text' placeholder = 'Name' name = 'from_name'>
+            <input className = 'input-text' type = 'text' placeholder = 'Name' name = 'name' value = {value.name} onChange={handleChange} required>
             </input> 
-            <input className = 'input-text' type = 'email' placeholder = 'Email' name = 'email_address'>
+            <input className = 'input-text' type = 'email' placeholder = 'Email' name = 'email' value = {value.email} onChange={handleChange} required>
             </input>
-            <textarea className = 'input-text' type = 'text' placeholder = 'Message' name = 'message'>
+            <textarea className = 'input-text' type = 'text' placeholder = 'Message' name = 'message' value = {value.message} onChange={handleChange} required>
             </textarea>
         </div> 
-        <button className = 'btn' type = 'submit' value = 'submit' onClick = {toggle}>SEND</button>
+        <button className = 'btn' type = 'submit' value = 'submit' >SEND</button>
     </form>
 </div>   
 )
