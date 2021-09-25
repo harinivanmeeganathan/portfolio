aca28cba91030a3d1adbd2d367696ca99a34b693
harinivanmeeganathan <harinivanmeeganathan@gmail.com>
2021-09-25 20:34:43 +0530

    indexjs file

--- index.js
+++ index.js
@@ -42,10 +42,10 @@ app.get('/api/mail', async(req, res) => {
 }
 })
 
-app.use(express.static('client/build'));
+app.use(express.static('Client/build'));
 if (process.env.NODE_ENV === 'production') {
     app.get('/*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, './client', 'build', 'index.html'))
+        res.sendFile(path.resolve(__dirname, './Client', 'build', 'index.html'))
     });
 }
 
